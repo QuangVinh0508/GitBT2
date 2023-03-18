f3f8831469e934d8782b3aef2975455f392aa127
Quang Vinh <tranquangvinh2017tv@gmail.com>
2023-03-18 08:05:01 +0700

    Fix js.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,8 @@
-// Change box1 -> box2
-var box1 = document.querySelector('.box1');
-box1.onclick = function () {
-    box1.style.display = "none";
-    box2.style.display = "block";
-    box3.style.display = "none";
-};
-
-// Change box2 -> box3
-var box2 = document.querySelector('.box2');
-box2.onclick = function () {
-    box1.style.display = "none";
-    box2.style.display = "none";
-    box3.style.display = "block";
-};
-
-// Change box3 -> box1
-var box3 = document.querySelector('.box3');
-box3.onclick = function () {
-    box1.style.display = "block";
-    box2.style.display = "none";
-    box3.style.display = "none";
-};
+function handleColorChange() {
+    var boxList = document.querySelectorAll('.box');
+  
+    var tmp = boxList[2].style.backgroundColor
+    boxList[2].style.backgroundColor = boxList[1].style.backgroundColor;
+    boxList[1].style.backgroundColor = boxList[0].style.backgroundColor;
+    boxList[0].style.backgroundColor = tmp;
+  }  
